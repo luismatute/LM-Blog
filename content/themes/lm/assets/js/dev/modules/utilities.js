317f19fceff123f7e5aea9a71949111e0dc0bd4b
LM10 <luis.matute@me.com>
2014-04-12 10:33:06 -0600

    -Fix GA

--- content/themes/lm/assets/js/dev/modules/utilities.js
+++ content/themes/lm/assets/js/dev/modules/utilities.js
@@ -78,10 +78,13 @@
 
 		// Google Analytics: change UA-XXXXX-X to be your site's ID.
 			gaSetup: function( account_id ) {
-		        var _gaq=[['_setAccount',account_id],['_trackPageview']];
-		        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		        g.src=('https:'===location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-		        s.parentNode.insertBefore(g,s)}(document,'script'));
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+				console.log(account_id);
+				ga('create', account_id, 'luismatute.me');
+				ga('send', 'pageview');
 		    }
 	};
 
